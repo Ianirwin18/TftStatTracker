a554ad6368e69f0b6d2b3d6840a547df1d5a1c18
John Hull <john.alex.hull@gmail.com>
2022-07-05 18:23:08 -0700

    seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,275 @@
 const champs = require('../seeds/merge.js');
+
+INSERT INTO champs (champion,cost ,trait1 ,trait2 ,trait3 )
+VALUES (
+     'Aatrox',
+     1,
+     'Shimmerscale',
+     'Warrior'
+  ),
+  (
+     'Anivia',
+     3,
+     'Jade',
+     'Evoker',
+     'Legend'
+  ),
+  (
+     'Ao Shin',
+     10,
+     'Tempest',
+     'Dragon'
+  ),
+  (
+     'Ashe',
+     2,
+     'Jade',
+     'Swiftshot',
+     'Dragonmancer'
+  ),
+  (
+     'Aurelion Sol',
+     10,
+     'Astral',
+     'Dragon',
+     'Evoker'
+  ),
+  (
+     'Bard',
+     5,
+     'Guild',
+     'Mystic',
+     'Bard'
+  ),
+  (
+     'Braum',
+     2,
+     'Scalescorn',
+     'Guardian'
+  ),
+  (  'Corki',  4,  'Revel',  'Cannoneer' ),
+  (  'Daeja',  8,  'Mirage',  'Dragon' ),
+  (
+     'Diana',
+     3,
+     'Scalescorn',
+     'Assassin'
+  ),
+  (
+     'Elise',
+     3,
+     'Whispers',
+     'Shapeshifter'
+  ),
+  (
+     'Ezreal',
+     1,
+     'Tempest',
+     'Swiftshot'
+  ),
+  (  'Gnar',  2,  'Jade',  'Shapeshifter' ),
+  (
+     'Hecarim',
+     4,
+     'Ragewing',
+     'Cavalier'
+  ),
+  (
+     'Heimerdinger',
+     1,
+     'Trainer',
+     'Mage'
+  ),
+  (
+     'Idas',
+     8,
+     'Shimmerscale',
+     'Guardian',
+     'Dragon'
+  ),
+  (  'Illaoi',  3,  'Astral',  'Bruiser' ),
+  (  'Jinx',  2,  'Revel',  'Cannoneer' ),
+  (
+     'Karma',
+     1,
+     'Jade',
+     'Dragonmancer'
+  ),
+  (
+     'Kayn',
+     2,
+     'Ragewing',
+     'Shimmerscale',
+     'Assassin'
+  ),
+  (
+     'Lee Sin',
+     3,
+     'Tempest',
+     'Dragonmancer'
+  ),
+  (  'Leona',  1,  'Mirage',  'Guardian' ),
+  (
+     'Lillia',
+     2,
+     'Scalescorn',
+     'Cavalier',
+     'Mage'
+  ),
+  (
+     'Lulu',
+     3,
+     'Trainer',
+     'Mystic',
+     'Evoker'
+  ),
+  (
+     'Nami',
+     2,
+     'Astral',
+     'Mystic',
+     'Mage'
+  ),
+  (
+     'Neeko',
+     4,
+     'Jade',
+     'Shapeshifter'
+  ),
+  (
+     'Nidalee',
+     1,
+     'Astral',
+     'Shapeshifter'
+  ),
+  (  'Nunu',  3,  'Mirage',  'Cavalier' ),
+  (
+     'Olaf',
+     3,
+     'Scalescorn',
+     'Bruiser',
+     'Warrior'
+  ),
+  (
+     'Ornn',
+     4,
+     'Tempest',
+     'Bruiser',
+     'Legend'
+  ),
+  (  'Pyke',  5,  'Whispers',  'Assassin' ),
+  (
+     'Qiyana',
+     2,
+     'Tempest',
+     'Assassin'
+  ),
+  (  'Ryze',  3,  'Guild',  'Mage' ),
+  (  'Sejuani',  1,  'Guild',  'Cavalier' ),
+  (
+     'Senna',
+     1,
+     'Ragewing',
+     'Cannoneer'
+  ),
+  (
+     'Sett',
+     1,
+     'Ragewing',
+     'Dragonmancer'
+  ),
+  (
+     'Shen',
+     2,
+     'Ragewing',
+     'Bruiser',
+     'Warrior'
+  ),
+  (
+     'Shi Oh Yu',
+     8,
+     'Jade',
+     'Mystic',
+     'Dragon'
+  ),
+  (
+     'Shyvana',
+     10,
+     'Ragewing',
+     'Dragon',
+     'Shapeshifter'
+  ),
+  (  'Skarner',  1,  'Astral',  'Bruiser' ),
+  (  'Sona',  4,  'Revel',  'Evoker' ),
+  (  'Soraka',  5,  'Jade',  'Starcaller' ),
+  (
+     'Swain',
+     3,
+     'Ragewing',
+     'Dragonmancer',
+     'Shapeshifter'
+  ),
+  (
+     "Sy'fen",
+     8,
+     'Whispers',
+     'Bruiser',
+     'Dragon'
+  ),
+  (
+     'Sylas',
+     3,
+     'Whispers',
+     'Bruiser',
+     'Mage'
+  ),
+  (
+     'Tahm Kench',
+     1,
+     'Revel',
+     'Bruiser'
+  ),
+  (  'Talon',  4,  'Guild',  'Assassin' ),
+  (  'Taric',  1,  'Jade',  'Guardian' ),
+  (
+     'Thresh',
+     2,
+     'Whispers',
+     'Guardian'
+  ),
+  (
+     'Tristana',
+     2,
+     'Trainer',
+     'Cannoneer'
+  ),
+  (  'Twitch',  2,  'Guild',  'Swiftshot' ),
+  (  'Varus',  3,  'Astral',  'Swiftshot' ),
+  (  'Vladimir',  1,  'Astral',  'Mage' ),
+  (
+     'Volibear',
+     3,
+     'Shimmerscale',
+     'Dragonmancer',
+     'Legend'
+  ),
+  (
+     'Xayah',
+     4,
+     'Ragewing',
+     'Swiftshot'
+  ),
+  ( 'Yasuo',
+  5,
+   'Mirage',
+     'Dragonmancer',
+   'Warrior'
+  ),
+  (  'Yone',  2,  'Mirage',  'Warrior' ),
+  (
+     'Zoe',
+     5,
+     'Shimmerscale',
+     'Mage',
+     'Spell Thief'
+    )
