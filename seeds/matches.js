a7f23bc97492d34fc9bc454102ce0c83e83f5237
Justin Lee <98000043+justinlee781@users.noreply.github.com>
2022-07-09 06:50:34 -0700

    Match Id

--- seeds/matches.js
+++ seeds/matches.js
@@ -1,42 +1,60 @@
-const seed = require("./tonyfriendmatches.json");
+const seed = require("./mattmatches.json");
 const { Champion } = require("../models");
-const fs = require('fs')
+const fs = require("fs");
 
-const pointsMap = {
-    "1": 4,
-    "2": 3,
-    "3": 2,
-    "4": 1,
-    "5": -1,
-    "6": -2,
-    "7": -3,
-    "8": -4
-}
+const getMatchSeed = async (filename, userId) => {
+  const seed = require(filename);
 
-const hydrated = seed.map(a => {
+  const pointsMap = {
+    1: 4,
+    2: 3,
+    3: 2,
+    4: 1,
+    5: -1,
+    6: -2,
+    7: -3,
+    8: -4,
+  };
+
+  const hydrated = seed.map((a, i) => {
     const [place, ...champs] = a;
     return {
-        place,
-        score: pointsMap[place + ""],
-        champs
-    }
-})
+      place,
+      score: pointsMap[place + ""],
+      champs,
+      matchId: parseInt(`${userId}${i + 1}`),
+    };
+  });
 
-console.log(hydrated);
+  const champions = await Champion.findAll();
+  const championsSerial = champions.map((a) => a.get({ plain: true }));
+  // console.log(championsSerial);
+  const champNameMap = championsSerial.reduce(
+    (a, b) => ((a[b.champion] = b), a),
+    {}
+  );
+  const matchChamps = hydrated.reduce((col, match, i) => {
+    // console.log("INDEX", match);
+    const champs = match.champs.map((name, index) => ({
+      place: match.place,
+      score: match.score,
+      matchId: match.matchId,
+      championId: champNameMap[name].id,
+    }));
+    return [...col, ...champs];
+  }, []);
+  const userMatch = hydrated.map((match) => {
+    return {
+      user_id: userId,
+      id: match.matchId,
+      place: match.place,
+      score: match.score,
+    };
+  });
+  return { matchChamps, userMatch };
+};
 
+module.exports = getMatchSeed;
 
-(async () => {
-    const champions = await Champion.findAll();
-    const championsSerial = champions.map(a => a.get({ plain: true }));
-    // console.log(championsSerial);
-    const champNameMap = championsSerial.reduce((a, b) => (a[b.champion] = b, a), {});
-    const matchChamps = hydrated.reduce((col, match, i) => {
-        console.log("INDEX", match)
-        const champs = match.champs.map((name) => ({place: match.place, score:match.score, match_id: i+1, champion_id: champNameMap[name].id }));
-        return [...col,...champs]
-    }, [])
-
-    fs.writeFileSync("./champmatchseed.json", JSON.stringify(matchChamps))
-})()
 
 
