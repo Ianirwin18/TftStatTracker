6109afd96cb5586aeacebeae2e481b66fb23b505
Ian Irwin <Ianiriwn18@gmail.com>
2022-07-08 19:39:23 -0700

    wip ian

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 //These are all the view routes for our application
 const router = require('express').Router();
+const {Champion} = require("../models")
 
 //when a GET request is received on the root(/) route,
 //render the home.handlebars view
@@ -18,7 +19,9 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render('signup');
 });
-router.get('/stats', (req, res) => {
-  res.render('stats');
+router.get('/stats', async(req, res) => {
+  const champs = await Champion.findAll();
+  const champions = champs.map(c => c.get({plain:true}));
+  res.render('stats', {champions});
 });
 module.exports = router;
